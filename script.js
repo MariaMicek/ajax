b6bc8aa33388fa95863cca8922f30ef9651ab3f2
Maria Micek <mmicek16@wp.pl>
2019-03-26 14:36:12 +0100

    promise basis - different examples

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-class NumbersContainer {
-    constructor(){
-        this.numbers = []
-    }
+const promise = fetch('https://randomuser.me/api')
 
-    addNumber(number){
-        this.numbers = this.numbers.concat(number)
-        return this
-    }
-}
+// 1
+promise
+    .then((promiseValue) => console.log(promiseValue))
 
-const container1 = new NumbersContainer()
 
-container1.addNumber(4)
-console.log(JSON.stringify(container1))
-console.log(container1.numbers)
+// 2
+let response = null
 
-container1.addNumber(6)
-console.log(container1)
-console.log(container1.numbers)
+    promise
+    .then((promiseValue) => response = promiseValue)
 
-container1.addNumber(4).addNumber(9).addNumber(11)
-console.log(container1)
-console.log(container1.numbers)
+console.log(response)       //wyświtli nadal null bo console.log wykona się przed promise, dlatego nie da się dobrać poza then do wartości promise
+
+
+//3
+promise
+    .then((promiseValue) => console.log(promiseValue.json()))       //również zawraca promise, ale zamienia dane na sparsowany JSON, czyli obiekt 
+
+
+
+//4
+promise                                 
+    .then( promiseValue => {            //promise można zagnieżdżać
+        promiseValue.json()
+            .then( data => {
+                console.log(data)
+            })
+    }) 
+    .then( valueFromPromiseFromThen => {            //następne then biorą funkcje z wartością z poprzednich then lub rozwiązanie z promisy poprzedniego then
+        console.log(valueFromPromiseFromThen)
+    })
+
+
+//5
+promise
+    .then(response => response.json())      //to samo co wyżej, tylko prościej - zwraca dane z promisa
+    .then(data => console.log(data))
+    .catch(error => console.log('Error', error))             //wyświetla w przypadku błędu, wtedy kolejne theny się nie wywołują, tylko od razu catch
+    .finally(() => console.log('I will always be here!'))
+
+
+//6
+
+
+const promise = fetch('https://randomuser.me/api')
+
+const writeInBody = message => document.body.innerText = message        //wyświetlanie komunikatów w różnych przypadkach działania promisy
+
+writeInBody('Oczekuję na dane')
+
+promise
+    .then(response => 
+        response.json()
+    )      
+    .then(data => {
+        console.log(data.results[0].name)
+        document.body.innerText = (
+            data.results[0].name.first +
+            ' ' +
+            data.results[0].name.last
+        )
+      
+    })
+    .catch(() => writeInBody('Wystąpił błąd'))
